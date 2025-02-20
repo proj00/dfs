f845e892e45416f91df7701e886f2f2c11285205
pppss102 <73379683+pppss102@users.noreply.github.com>
2025-02-20 19:13:11 +0200

    add dotnet test projects

---
+++ dfs/tracker-unit-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace tracker_unit_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
