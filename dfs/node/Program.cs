35d5aae9bfbdc5a6dfc673bf18139b1302376ae7
pppss102 <73379683+pppss102@users.noreply.github.com>
2025-02-25 22:01:01 +0200

    add webview2

--- dfs/node/Program.cs
+++ dfs/node/Program.cs
@@ -1,24 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace node
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            global::System.Windows.Forms.Application.EnableVisualStyles();
+            global::System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            global::System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            System.Windows.Forms.Application.Run(new UI());
+        }
+    }
 }
-
-app.MapGet("/empty", () =>
-{
-    return "";
-})
-.WithName("GetEmpty")
-.WithOpenApi();
-
-app.Run();
---
+++ dfs/node/UI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace node
+{
+    public partial class UI : Form
+    {
+        public UI()
+        {
+            InitializeComponent();
+#if DEBUG
+            webView.Source = new Uri("localhost:59102", UriKind.Absolute);
+#else
+            webView.Source = new Uri("https://www.google.com", UriKind.Absolute);
+#endif
+            ((System.ComponentModel.ISupportInitialize)webView).EndInit();
+            ResumeLayout(false);
+        }
+    }
+}
