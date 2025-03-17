04b32d1cba0e49b98b818c88c2b44ace2fce0491
a <a@a>
2025-03-17 19:06:50 +0200

    fix INodeService interface

--- dfs/ui/src/IpcService/INodeService.ts
+++ dfs/ui/src/IpcService/INodeService.ts
@@ -7,6 +7,7 @@ export interface INodeService {
   RegisterUiService: (service: any) => Promise<void>;
   PickObjectPath: (folder: boolean) => Promise<string>;
   ImportObjectFromDisk: (path: string, chunkSize: number) => Promise<void>;
+  PublishToTracker: (hashes: string[], trackerUri: string) => Promise<void>;
 }
 
 // make TS shut up (again; UI::browser makes this available after BindObjectAsync)
