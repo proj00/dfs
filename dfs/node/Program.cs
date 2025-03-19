f5d29020d99c57fed15b5b43dda5fbca5574cb84
a <a@a>
2025-03-19 19:22:40 +0200

    finally fix object downloads

--- dfs/node/Program.cs
+++ dfs/node/Program.cs
@@ -6,6 +6,9 @@ using CefSharp;
 using node.UiResourceLoading;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Grpc.Core;
+using System.Threading.Tasks;
+using common_test;
 
 namespace node
 {
@@ -18,29 +21,65 @@ namespace node
         [STAThread]
         static void Main()
         {
+            GrpcEnvironment.SetLogger(new Grpc.Core.Logging.LogLevelFilterLogger(
+                new Grpc.Core.Logging.ConsoleLogger(),
+                Grpc.Core.Logging.LogLevel.Debug));
+
             NodeState state = new(TimeSpan.FromMinutes(1));
             NodeRpc rpc = new(state);
-            var server = new Grpc.Core.Server() { Services = { Node.Node.BindService(rpc) } };
+            var server = new Grpc.Core.Server()
+            {
+                Services = { Node.Node.BindService(rpc) },
+                Ports = { new ServerPort("localhost", 0, ServerCredentials.Insecure) }
+            };
+
             server.Start();
+
+            foreach (var port in server.Ports)
+            {
+                Console.WriteLine($"Server is listening on {port.Host}:{port.BoundPort}");
+            }
 
             NodeService service = new(state, rpc);
 
-            CefSharpSettings.ConcurrentTaskExecution = true;
-#if !DEBUG
-            var settings = new CefSettings();
-            settings.RegisterScheme(new CefCustomScheme()
+
+            if (false) // mock for demo
             {
-                SchemeName = "http",
-                DomainName = "ui.resources",
-                SchemeHandlerFactory = new UiResourceHandlerFactory(),
-            });
-            Cef.Initialize(settings);
+                var file = service.PickObjectPath(true);
+                var hash = service.ImportObjectFromDisk(file, 1024);
+
+                var tracker = new MockTrackerWrapper();
+                tracker.peerId = $"http://127.0.0.1:{server.Ports.First().BoundPort}";
+                service.PublishToTracker(state.objectByHash.Keys.ToArray(), tracker).Wait();
+
+                if (Console.ReadLine() == "aa")
+                {
+                    var folder = service.PickObjectPath(true);
+
+                    service.DownloadObjectByHash(hash, tracker, folder).Wait();
+                    Console.WriteLine("Press Enter to continue...");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                CefSharpSettings.ConcurrentTaskExecution = true;
+#if !DEBUG
+                        var settings = new CefSettings();
+                        settings.RegisterScheme(new CefCustomScheme()
+                        {
+                            SchemeName = "http",
+                            DomainName = "ui.resources",
+                            SchemeHandlerFactory = new UiResourceHandlerFactory(),
+                        });
+                        Cef.Initialize(settings);
 #endif
 
-            global::System.Windows.Forms.Application.EnableVisualStyles();
-            global::System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-            global::System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            System.Windows.Forms.Application.Run(new UI(service));
+                global::System.Windows.Forms.Application.EnableVisualStyles();
+                global::System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+                global::System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                System.Windows.Forms.Application.Run(new UI(service));
+            }
 
             server.ShutdownAsync().Wait();
         }
