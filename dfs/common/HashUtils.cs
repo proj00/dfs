efa49907582687bf1b1a392dc7884682fa71511e
a <a@a>
2025-03-19 19:19:06 +0200

    add tracker mocking

--- dfs/common/HashUtils.cs
+++ dfs/common/HashUtils.cs
@@ -2,45 +2,48 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto.Digests;
 
 namespace common
 {
     public static class HashUtils
     {
-        public static string GetHash(byte[] data)
+        private static byte[] GetFinal(Sha3Digest digest)
         {
-            var hash = SHA3_512.HashData(data);
-            var hex = Convert.ToHexStringLower(hash);
-
-            return hex;
+            var hash = new byte[digest.GetDigestSize()];
+            digest.DoFinal(hash, 0);
+            return hash;
         }
 
-        public static string GetHash(Stream data)
+        public static string GetHash(byte[] data)
         {
-            var hash = SHA3_512.HashData(data);
-            var hex = Convert.ToHexStringLower(hash);
+            var digest = new Sha3Digest(512);
+            digest.BlockUpdate(data);
+            var hex = Convert.ToHexStringLower(GetFinal(digest));
 
             return hex;
         }
 
         public static string GetHash(ReadOnlySpan<byte> data)
         {
-            var hash = SHA3_512.HashData(data);
-            var hex = Convert.ToHexStringLower(hash);
+            var digest = new Sha3Digest(512);
+            digest.BlockUpdate(data);
+            var hex = Convert.ToHexStringLower(GetFinal(digest));
 
             return hex;
         }
 
         public static string GetHash(Fs.FileSystemObject obj)
         {
-            var hash = SHA3_512.HashData(obj.ToByteArray());
-            var hex = Convert.ToHexStringLower(hash);
+            var digest = new Sha3Digest(512);
+            digest.BlockUpdate(obj.ToByteArray());
+            var hex = Convert.ToHexStringLower(GetFinal(digest));
 
             return hex;
         }
+
         public static string CombineHashes(string[] hashes)
         {
             var data = hashes.Select(hash => Convert.FromHexString(hash));
