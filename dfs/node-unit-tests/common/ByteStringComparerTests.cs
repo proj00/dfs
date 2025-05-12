c717e79e96ddddab1938b42a6d21ff98f131ae13
a <73379683+pppss102@users.noreply.github.com>
2025-05-12 23:43:36 +0300

    fix bytestringcomparer tests

--- dfs/node-unit-tests/common/ByteStringComparerTests.cs
+++ dfs/node-unit-tests/common/ByteStringComparerTests.cs
@@ -27,5 +27,26 @@ namespace unit_tests.common
             var str1 = ByteString.CopyFrom(faker.Random.Bytes(1023));
             Assert.That(comparer.Compare(str, str1), Is.Not.EqualTo(0));
         }
+
+        [Test]
+        public void TestCompare_NotEqualSizes_Throws()
+        {
+            var comparer = new ByteStringComparer();
+            var str = ByteString.CopyFrom(faker.Random.Bytes(1023));
+            var str1 = ByteString.CopyFrom(faker.Random.Bytes(1024));
+            var ex = Assert.Throws<ArgumentException>(() => comparer.Compare(str, str1));
+            Assert.That(ex.Message, Does.Contain("Compare failed; invalid arguments (mismatched ByteString lengths)"));
+        }
+
+        [Test]
+        public void TestCompare_NullArgs_Throws([Values] bool useLhs, [Values] bool useRhs)
+        {
+            var comparer = new ByteStringComparer();
+            var lhs = useLhs ? ByteString.CopyFrom(faker.Random.Bytes(1023)) : null;
+            var rhs = useRhs ? ByteString.CopyFrom(faker.Random.Bytes(1024)) : null;
+
+            if (!useLhs || !useRhs)
+                Assert.Throws<ArgumentNullException>(() => comparer.Compare(lhs, rhs));
+        }
     }
 }
