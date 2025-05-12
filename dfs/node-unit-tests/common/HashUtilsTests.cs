ea7d40df6b80f81efa671085434abf9415cac332
a <73379683+pppss102@users.noreply.github.com>
2025-05-13 00:28:12 +0300

    ??

--- dfs/node-unit-tests/common/HashUtilsTests.cs
+++ dfs/node-unit-tests/common/HashUtilsTests.cs
@@ -1,10 +1,12 @@
 ﻿using common;
+using Google.Protobuf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Animation;
+using unit_tests.node;
 
 namespace unit_tests.common
 {
@@ -21,6 +23,20 @@ namespace unit_tests.common
             var h2 = HashUtils.GetHash(buf);
 
             Assert.That(h1, Is.EqualTo(h2));
+        }
+        [Test]
+        public void Test_GetObjectHash_IsConsistent()
+        {
+            var obj = MockFsUtils.GenerateObject(faker);
+            var set = new HashSet<ByteString>(new ByteStringComparer());
+
+            // protobuf serialization is messy sometimes, doublecheck
+            for (int i = 0; i < 128; i++)
+            {
+                set.Add(HashUtils.GetHash(obj.Object));
+            }
+
+            Assert.That(set, Has.Count.EqualTo(1));
         }
 
         [Test]
