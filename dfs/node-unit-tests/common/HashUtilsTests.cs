01bb9aaffc25cc560e2b1e0a14f8200769ec13d2
a <73379683+pppss102@users.noreply.github.com>
2025-05-13 00:24:18 +0300

    consistence of hashing...

---
+++ dfs/node-unit-tests/common/HashUtilsTests.cs
@@ -0,0 +1,38 @@
+﻿using common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace unit_tests.common
+{
+    class HashUtilsTests
+    {
+        private static readonly Bogus.Faker faker = new();
+
+        [Test]
+        public void Test_GetHash_IsConsistent()
+        {
+            var buf = faker.Random.Bytes(10);
+            ReadOnlySpan<byte> span = new(buf);
+            var h1 = HashUtils.GetHash(span);
+            var h2 = HashUtils.GetHash(buf);
+
+            Assert.That(h1, Is.EqualTo(h2));
+        }
+
+        [Test]
+        public void Test_MultipleHashes_IsConsistent()
+        {
+            var buf = faker.Random.Bytes(10);
+            ReadOnlySpan<byte> span = new(buf);
+            var h1 = HashUtils.GetHash(span);
+            var h2 = HashUtils.GetHash(buf);
+
+            Assert.That(HashUtils.CombineHashes([h1, h2]),
+                Is.EqualTo(HashUtils.GetHash(HashUtils.ConcatHashes([h1, h2]).ToByteArray())));
+        }
+    }
+}
