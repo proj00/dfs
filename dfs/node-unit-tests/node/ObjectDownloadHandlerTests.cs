60e2805e091c0bd55080d4eb55524f1287715042
a <73379683+pppss102@users.noreply.github.com>
2025-05-13 22:43:40 +0300

    ??

--- dfs/node-unit-tests/node/ObjectDownloadHandlerTests.cs
+++ dfs/node-unit-tests/node/ObjectDownloadHandlerTests.cs
@@ -22,7 +22,7 @@ namespace unit_tests.node
         private static readonly Bogus.Faker faker = new();
         [Test]
         [CancelAfter(1000)]
-        public async Task TestAddObjectFromDiskAsync(CancellationToken token)
+        public async Task TestAddObjectFromDiskAsync([Values] bool useFolder, CancellationToken token)
         {
             var contents = faker.Random.Bytes(1024);
             GrpcChannelFactory factory = (Uri uri, GrpcChannelOptions opt) =>
@@ -53,21 +53,61 @@ namespace unit_tests.node
                 new Mock<INativeMethods>().Object
                 );
 
-            var (objectArray, rootHash) = await handler.AddObjectFromDiskAsync("./root_dir", 1024);
+            var (objectArray, rootHash) = await handler.AddObjectFromDiskAsync(useFolder ? "./root_dir" : "C:/root_dir/subdir/file", 1024);
             var objects = objectArray.ToList();
             using (Assert.EnterMultipleScope())
             {
-                Assert.That(objects, Has.Count.EqualTo(3));
-                var root = objects.Find(o => o.Object.Name == "root_dir");
-                var subdir = objects.Find(o => o.Object.Name == "subdir");
+                Assert.That(objects, Has.Count.EqualTo(useFolder ? 3 : 1));
                 var file = objects.Find(o => o.Object.Name == "file");
-                Assert.That(root, Is.Not.Null);
-                Assert.That(subdir, Is.Not.Null);
                 Assert.That(file, Is.Not.Null);
-
-                Assert.That(root.Object.Directory.Entries, Does.Contain(subdir.Hash));
-                Assert.That(subdir.Object.Directory.Entries, Does.Contain(file.Hash));
+                if (useFolder)
+                {
+                    var root = objects.Find(o => o.Object.Name == "root_dir");
+                    var subdir = objects.Find(o => o.Object.Name == "subdir");
+                    Assert.That(root, Is.Not.Null);
+                    Assert.That(subdir, Is.Not.Null);
+                    Assert.That(root.Object.Directory.Entries, Does.Contain(subdir.Hash));
+                    Assert.That(subdir.Object.Directory.Entries, Does.Contain(file.Hash));
+                }
             }
+        }
+
+        [Test]
+        [CancelAfter(1000)]
+        public async Task TestDownloadObjectByHashAsync(CancellationToken token)
+        {
+            var contents = faker.Random.Bytes(1024);
+            GrpcChannelFactory factory = (Uri uri, GrpcChannelOptions opt) =>
+            {
+                return new MockChannel(uri.ToString());
+            };
+
+            using var clientHandler = new GrpcClientHandler(TimeSpan.FromMinutes(1),
+                factory,
+                new Mock<ILoggerFactory>().Object);
+
+            var fs = new MockFileSystem(
+                new Dictionary<string, MockFileData>()
+                {
+                    {"root_dir/subdir/file", new(contents) }
+                },
+                new MockFileSystemOptions() { CurrentDirectory = "C:/" });
+
+            var handler = new ObjectDownloadHandler
+                (
+                fs,
+                new Mock<ILogger>().Object,
+                new Mock<IFilePathHandler>().Object,
+                clientHandler,
+                new Mock<IDownloadManager>().Object,
+                new Mock<IAsyncIOWrapper>().Object,
+                new Mock<IFilesystemManager>().Object,
+                new Mock<INativeMethods>().Object
+                );
+
+            var tracker = new MockTrackerWrapper();
+
+            //await handler.DownloadObjectByHashAsync("./root_dir", 1024);
         }
     }
 }
